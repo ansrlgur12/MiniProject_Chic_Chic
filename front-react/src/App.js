4d9b41458092f168fd2c495beb25b4c2d7336988
10ESR10 <kyjin115@naver.com>
2023-04-20 21:02:49 +0900

    이미지테스트

--- front-react/src/App.js
+++ front-react/src/App.js
@@ -7,9 +7,14 @@ import ImageTest from './pages/imageTest/imageTest';
 import ImageTest1 from './pages/imageTest/imageTest1';
 import TestMain from './pages/imageTest/TestMain';
 import ImageTest2 from './pages/imageTest/imageTest2';
+import ImageTest3 from './pages/imageTest/imageTest3';
+import ImageTest4 from './pages/imageTest/imageTest4';
+import ImageTest5 from './pages/imageTest/imageTest5';
+import ImageTest6 from './pages/imageTest/imageTest6';
 import Information from './pages/Infomation';
 import UserDeal from './pages/Userdeal';
 import Article from './pages/Article';
+
 
 
 function App() {
@@ -25,6 +30,10 @@ function App() {
         <Route path='/article' element={<Article />} />
         <Route path='/imagetest1' element={<ImageTest1 />} />
         <Route path='/imagetest2' element={<ImageTest2 />} />
+        <Route path='/imagetest3' element={<ImageTest3 />} />
+        <Route path='/imagetest4' element={<ImageTest4 />} />
+        <Route path='/imagetest5' element={<ImageTest5 />} />
+        <Route path='/imagetest6' element={<ImageTest6 />} />
 
 
       </Routes>
--- front-react/src/pages/imageTest/imageTest2.jsx
+++ front-react/src/pages/imageTest/imageTest2.jsx
@@ -1,15 +1,52 @@
 import React from "react";
 import Header from "../../Header/Header";
-import { Gage,ImageTestStyle } from "./imageTest";
+import { Gage,ImageTestStyle,ImageTestStyle1 ,TextOpt} from "./imageTest";
+import { useNavigate } from "react-router-dom";
 
 
 
 const ImageTest2 = () => {
+    const nav= useNavigate();
     
 
     return(
         <>
         <Header/>
+        <ImageTestStyle>
+            <ImageTestStyle1>
+        <div class="container">
+        <Gage>
+            <div className="gage">
+          <p className="qnum">3/6</p>
+        <div class="qheader">
+          <div className="bar" style={{width:' 10vw',height:'100%', background:'black'}}></div>
+        </div>
+        </div>
+        </Gage>
+        <div class="content">
+          <div class="question">
+            <h2 class="question-title">질문 3</h2>
+            <p class="question-description">어떤 향수를 선호하세요?</p>
+            </div>
+            <div class="options">
+             
+            <TextOpt>
+              <button class="text-option" onClick={()=>nav("/imageTest3")}>어디선가 맡아본 익숙한 향수</button>
+              
+             
+              
+              <button class="text-option" onClick={()=>nav("/imageTest3")}>흔하게 맡아 볼 수 없던 향수</button>
+              
+             
+            
+            </TextOpt>
+            </div>
+          </div>
+          
+        </div>
+      
+      </ImageTestStyle1>
+      </ImageTestStyle>
         
           
 
