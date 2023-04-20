f579f4e70c562ba41219ff055f76c1f4059da4f8
10ESR10 <kyjin115@naver.com>
2023-04-20 12:29:34 +0900

    테스트 포지션 변경

--- front-react/src/pages/imageTest/imageTest.jsx
+++ front-react/src/pages/imageTest/imageTest.jsx
@@ -18,7 +18,7 @@ export const ImageTestStyle = styled.div`
     display:flex;
     align-items:center;
     justify-content:center;
-   
+    
     
   
   
@@ -29,7 +29,7 @@ export const ImageTestStyle = styled.div`
     margin: 0px auto;
     padding: 2rem;
     margin-top:30vh;
-    position: relative;
+   
     display: flex;
   flex-direction: column;
   justify-content: center; 
