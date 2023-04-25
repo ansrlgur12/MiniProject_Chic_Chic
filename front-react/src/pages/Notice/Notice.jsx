06377d0cca8b2d163fd60c5d94fd39a8dafa8623
jis37 <jis8525@naver.com>
2023-04-25 14:08:45 +0900

    폴더배치2

--- front-react/src/pages/Notice/Notice.jsx
+++ front-react/src/pages/Notice/Notice.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import styled, {css} from "styled-components";
-import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
+import Header from "../../Header/Header";
+import Footer from "../../Footer/Footer";
+
 
 
 export const NoticeStyle = styled.div`
